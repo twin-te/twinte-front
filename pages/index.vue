c38a37715c754aa11e443ed75266de8b248bc82e
Sosuke Suzuki <aosukeke@gmail.com>
2019-05-13 14:04:08 +0900

    sassとnuxt-property-decoratorを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
   <h1>Twin:te</h1>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component
+export default class Index extends Vue {}
+</script>
+
+<style lang="sass" scoped>
+h1
+  color: red
+</style>
