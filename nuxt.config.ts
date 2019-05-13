f03b8480b47ba91235014e964dac3437cead3453
Sosuke Suzuki <aosukeke@gmail.com>
2019-05-13 14:19:50 +0900

    Add head tag to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 import NuxtConfiguration from "@nuxt/config";
 
 const config: NuxtConfiguration = {
+  head: {
+    titleTemplate: "Twinte",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
+    ]
+  },
   modules: ["@nuxtjs/vuetify"]
 };
 
