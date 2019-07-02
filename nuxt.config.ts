efea4eb56ad833656ef8c405b92f8e63ff17b7f1
HikaruEgashira <shighten@gmail.com>
2019-07-03 00:00:04 +0900

    add: normalize.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,22 @@
 import NuxtConfiguration from "@nuxt/config";
 
 const config: NuxtConfiguration = {
+  srcDir: "src/",
   head: {
     titleTemplate: "Twinte",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" }
+    ],
+    link: [
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+      }
     ]
   },
-  modules: ["@nuxtjs/vuetify"]
+  modules: []
 };
 
 export default config;
