9bc2f3ebc60d766f560a09850d710810c4062147
HikaruEgashira <shighten@gmail.com>
2019-07-15 22:28:16 +0900

    import timeTable

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,35 @@
 <template>
-  <h1>Twin:te</h1>
+  <section>
+    <div id="sem"><span id="notation">秋A</span></div>
+    <time-table id="table" semester></time-table>
+  </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
 
-@Component
-export default class Index extends Vue {}
+@Component({
+  components: {
+    timeTable: () => import("~/components/pages/table.vue")
+  }
+})
+export default class Index extends Vue {
+  public semester: number = 0;
+}
 </script>
 
 <style lang="sass" scoped>
-h1
-  color: red
+#notation
+  position: absolute
+  width: 26px
+  height: 23px
+  left: 176px
+  top: 64px
+  font-family: Noto Sans JP
+  font-style: normal
+  font-weight: 500
+  font-size: 16px
+  line-height: 23px
+  text-align: center
+  color: #9A9A9A
 </style>
