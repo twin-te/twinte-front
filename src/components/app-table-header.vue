8b91a81832e38862c1b5f69256d02e97ab5ac7fe
HikaruEgashira <shighten@gmail.com>
2019-08-21 01:13:47 +0900

    response

--- src/components/app-table-header.vue
+++ src/components/app-table-header.vue
@@ -1,74 +1,73 @@
 /** -> "../pages/index.vue" */
 <template>
+  <!-- 高さ 43px -->
   <section>
-    <div id="notation"><span @click="prevModule">く</span>{{ module }}<span @click="nextModule">く</span></div>
+    <div id="module">
+      <span @click="prevModule">く</span>
+      <span>{{ module }}</span>
+      <span @click="nextModule">く</span>
+    </div>
     <div id="week">
-      <div id="day"><app-day /></div>
+      <app-day id="day"></app-day>
       <div v-for="n in 5" :key="n">{{ week[n - 1] }}</div>
     </div>
   </section>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "nuxt-property-decorator";
-import * as Vuex from "vuex";
+import { Component, Vue } from 'nuxt-property-decorator'
+import * as Vuex from 'vuex'
 
 @Component({
   components: {
-    AppDay: () => import("~/components/app-day.vue")
-  }
+    AppDay: () => import('~/components/app-day.vue'),
+  },
 })
 export default class Index extends Vue {
-  $store!: Vuex.ExStore;
+  $store!: Vuex.ExStore
+
+  week: string[] = ['月', '火', '水', '木', '金']
 
   prevModule() {
-    this.$store.commit("table/prevModule");
+    this.$store.commit('table/prevModule')
   }
 
   nextModule() {
-    this.$store.commit("table/nextModule");
+    this.$store.commit('table/nextModule')
   }
 
-  get module() : string {
-    const num: number = this.$store.getters["table/moduleNum"];
-    const moduleListJp: string[] = ["春A", "春B", "春C", "秋A", "秋B", "秋C"];
-    return moduleListJp[num];
+  get module(): string {
+    const num: number = this.$store.getters['table/moduleNum']
+    const moduleListJp: string[] = ['春A', '春B', '春C', '秋A', '秋B', '秋C']
+    return moduleListJp[num]
   }
-
-  week: string[] = ["月", "火", "水", "木", "金"];
 }
 </script>
 
 <style lang="sass" scoped>
-#notation
-  position: absolute
-  width: 26px
-  height: 23px
-  left: 176px
-  top: 64px
+#module
+  display: flex
   font-family: Noto Sans JP
   font-style: normal
   font-weight: 500
   font-size: 16px
-  line-height: 23px
+  line-height: 16px
   color: #9A9A9A
+  justify-content: space-evenly
 #week
   display: flex
   justify-content: space-between
-  position: absolute
-  width: 243px
+  width: calc(100vw - 20px)
   height: 27px
-  left: 87px
-  top: 87px
+  margin-left: 10px
+  margin-right: 10px
+  line-height: 27px
   font-family: Noto Sans JP
   font-style: normal
   font-weight: 500
   font-size: 15px
   color: #9A9A9A
 #day
-  position: fixed
-  width: 49px
-  height: 48px
-  left: 14px
-  top: 62px
+  position: relative
+  top: -16px
 </style>
