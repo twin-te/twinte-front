eac07352b16ff2acbc231c6e2e187baa1a1f004a
HikaruEgashira <shighten@gmail.com>
2019-08-28 23:56:39 +0900

    lint
    Co-authored-by: Kanade Nishizawa <kanade27@icloud.com>

--- src/components/def-nav.vue
+++ src/components/def-nav.vue
@@ -5,8 +5,8 @@
     <transition name="slide">
       <nav class="main" v-if="drawer">
         設定
-        <section v-for="l in list" :key="l.id">
-          <ui-navlist>{{ l.name }}</ui-navlist>
+        <section class="menu-contents" v-for="l in list" :key="l.id">
+          <ui-navlist class="menu-content">{{ l.name }}</ui-navlist>
         </section>
       </nav>
     </transition>
@@ -18,33 +18,33 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "nuxt-property-decorator";
-import * as Vuex from "vuex";
+import { Component, Vue } from 'nuxt-property-decorator'
+import * as Vuex from 'vuex'
 
 @Component({
   components: {
-    "ui-navlist": () => import("~/components/ui-nav-list.vue")
-  }
+    'ui-navlist': () => import('~/components/ui-nav-list.vue'),
+  },
 })
 export default class Index extends Vue {
-  $store!: Vuex.ExStore;
+  $store!: Vuex.ExStore
 
   get drawer(): boolean {
-    return this.$store.getters["visible/drawer"];
+    return this.$store.getters['visible/drawer']
   }
 
   chDrawer(): void {
-    this.$store.commit("visible/chDrawer", { bool: false });
+    this.$store.commit('visible/chDrawer', { bool: false })
   }
 
   list: any = [
-    { name: "ホームへ戻る" },
-    { name: "使い方" },
-    { name: "About" },
-    { name: "表示設定" },
-    { name: "時間割の共有" },
-    { name: "時間割データの消去" }
-  ];
+    { name: 'ホームへ戻る', link: '/' },
+    { name: '使い方', link: '/tutorial' },
+    { name: 'About', link: '/about' },
+    { name: '表示設定', link: '/settings' },
+    { name: '時間割の共有', link: '/' },
+    { name: '時間割データの消去', link: '/' },
+  ]
 }
 </script>
 
@@ -53,8 +53,8 @@ export default class Index extends Vue {
   position: absolute
   left: 0px
   top: 0px
-  max-width: 300px
-  width: 80vw
+  max-width: 289px
+  width: 75vw
   height: 100vh
   box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.25)
   background: #fff
@@ -67,6 +67,18 @@ export default class Index extends Vue {
   height: 100vh
   background: rgba(100, 100, 100, 0.5)
   z-index: 5
+
+.menu-content
+  margin: 0
+  padding: auto
+  height: 65px
+  width: 263px
+  border-top: 1px solid #c4c4c4
+
+.menu-contents
+  position: absolute
+  margin: 136px 10px auto
+
 
 /** animation */
 .slide-enter-active, .slide-leave-active
