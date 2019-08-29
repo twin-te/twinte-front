68398d49cd2560ac99bd1ed1a632650660a68215
KanadeNishizawa <kanade27@icloud.com>
2019-08-30 01:20:28 +0900

    文字・アイコンの位置・色調整

--- src/components/def-nav.vue
+++ src/components/def-nav.vue
@@ -8,9 +8,9 @@
         <div class="login-btn">ログイン</div>
         <section class="menu-contents-wrap">
           <div class="menu-content" v-for="l in list" :key="l.id" :id="l.icon">
-            <span class="material-icons menuicon">{{ l.icon }}</span>
-            {{ l.name }}
-            <span class="material-icons">chevron_right</span>
+            <span class="material-icons menu-icon">{{ l.icon }}</span>
+            <p>{{ l.name }}</p>
+            <span class="material-icons menu-allow">chevron_right</span>
           </div>
         </section>
       </nav>
@@ -23,38 +23,39 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
-import * as Vuex from 'vuex'
+import { Component, Vue } from "nuxt-property-decorator";
+import * as Vuex from "vuex";
 
 @Component({
   components: {
-    'ui-navlist': () => import('~/components/ui-nav-list.vue'),
-  },
+    "ui-navlist": () => import("~/components/ui-nav-list.vue")
+  }
 })
 export default class Index extends Vue {
-  $store!: Vuex.ExStore
+  $store!: Vuex.ExStore;
 
   get drawer(): boolean {
-    return this.$store.getters['visible/drawer']
+    return this.$store.getters["visible/drawer"];
   }
 
   chDrawer(): void {
-    this.$store.commit('visible/chDrawer', { bool: false })
+    this.$store.commit("visible/chDrawer", { bool: false });
   }
 
   list: any = [
-    { icon: 'home', name: 'ホームへ戻る', link: '/' },
-    { icon: 'help', name: '使い方', link: '/tutorial' },
-    { icon: 'supervisor_account', name: 'About', link: '/about' },
-    { icon: 'view_quilt', name: '表示設定', link: '/settings' },
-    { icon: 'share', name: '時間割の共有', link: '/' },
-    { icon: 'delete_sweep', name: '時間割データの消去', link: '/' },
-  ]
+    { icon: "home", name: "ホームへ戻る", link: "/" },
+    { icon: "help", name: "使い方", link: "/tutorial" },
+    { icon: "supervisor_account", name: "About", link: "/about" },
+    { icon: "view_quilt", name: "表示設定", link: "/settings" },
+    { icon: "share", name: "時間割の共有", link: "/" },
+    { icon: "delete_sweep", name: "時間割データの消去", link: "/" }
+  ];
 }
 </script>
 
 <style lang="scss" scoped>
 .main {
+  font-family: Noto Sans JP;
   position: absolute;
   left: 0px;
   top: 0px;
@@ -75,10 +76,13 @@ export default class Index extends Vue {
   z-index: 5;
 }
 .settings {
-  margin: 12% auto 0 4%;
+  color: #717171;
+  font-size: 130%;
+  font-weight: 400;
+  margin: 12% auto 0 7%;
 }
 .login-btn {
-  margin: 8% 24px 0 24px;
+  margin: 8% auto 0 auto;
   text-align: center;
   line-height: 36px;
   max-width: 241px;
@@ -88,22 +92,46 @@ export default class Index extends Vue {
   color: #ffffff;
   border-radius: 6px;
 }
+.login-btn:hover {
+  background-color: #5e94ff;
+}
 .main .menu-contents-wrap {
-  margin: 4% 5% auto;
+  margin: 4% 5% auto 7%;
   max-width: 267px;
 }
 .menu-content {
+  position: relative;
   line-height: 65px;
   margin: 0;
   height: 65px;
   width: 100%;
   border-bottom: 1px solid #c4c4c4;
 }
-
-.menuicon {
-  vertical-align: center;
+.menu-icon {
+  color: #00c0c0;
+  position: absolute;
+  left: 3%;
+  top: 50%;
+  transform: translateY(-50%);
 }
-#home {
+.menu-content p {
+  color: #717171;
+  font-size: 105%;
+  font-weight: 300;
+  padding-left: 19%;
+  margin: 0;
+}
+.menu-allow {
+  color: #00c0c0;
+  position: absolute;
+  top: 50%;
+  right: 1%;
+  transform: translateY(-50%);
+}
+#home span {
+  color: #4380f8;
+}
+#home p {
   color: #4380f8;
 }
 /** animation */
