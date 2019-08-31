a11b16500bce0a59fc00a3782eed680bdc3ce68f
KanadeNishizawa <kanade27@icloud.com>
2019-08-31 17:21:29 +0900

    feat:pwa Co-authored-by: HikaruEgashira <shighten@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,44 @@
-import NuxtConfiguration from '@nuxt/config'
+import NuxtConfiguration from "@nuxt/config";
 
 const config: NuxtConfiguration = {
-  mode: 'universal',
-  srcDir: 'src/',
+  mode: "universal",
+  srcDir: "src/",
   head: {
-    titleTemplate: 'Twinte',
+    titleTemplate: "Twinte",
     htmlAttrs: {
-      lang: 'ja',
+      lang: "ja"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'robots', content: 'noindex' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "robots", content: "noindex" }
     ],
     link: [
       {
-        rel: 'stylesheet',
+        rel: "stylesheet",
         href:
-          'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
-      },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,500,700&display=swap&subset=japanese',
-      },
-    ],
+          "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+      }
+    ]
   },
-  css: ['@/assets/css/main.sass'],
-  modules: [],
-}
+  css: ["@/assets/css/main.scss"],
+  modules: ["@nuxtjs/pwa"],
+  workbox: {
+    dev: true
+  },
+  meta: {
+    mobileAppIOS: true,
+    appleStatusBarStyle: false,
+    lang: "ja",
+    nativeUI: true
+  },
+  manifest: {
+    name: "Twin:te",
+    short_name: "Twin:te",
+    lang: "ja",
+    start_url: "/",
+    display: "standalone"
+  }
+};
 
-export default config
+export default config;
