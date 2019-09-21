1a5af228df8050821ba5b1db256e2418095f5119
Hikaru Egashira <shighten@gmail.com>
2019-09-21 21:23:41 +0900

    feat: 教科リストを作成できるように

--- src/store/old_api/index.ts
+++ src/store/old_api/index.ts
@@ -1,137 +1,158 @@
-import axios from 'axios'
-import { union } from 'lodash'
+import axios from "axios";
+import union from "lodash/union";
 
-import tableData from '../../assets/json/data.json'
+import tableData from "../../assets/json/data.json";
 
 /** https://twinte.net/apiとの通信の状態管理を行なう */
-import { Getters, Mutations, Actions } from 'vuex'
-import { S, G, M, A } from './type'
+import { Getters, Mutations, Actions } from "vuex";
+import { S, G, M, A } from "./type";
 // ______________________________________________________
 //
 export const state = (): S => ({
   data: null,
   list_number: [],
-  moduleList: ['haruA', 'haruB', 'haruC', 'akiA', 'akiB', 'akiC'],
-})
+  moduleList: ["haruA", "haruB", "haruC", "akiA", "akiB", "akiC"]
+});
 // ______________________________________________________
 //
 export const getters: Getters<S, G> = {
   data(state) {
-    return state.data
+    return state.data;
   },
   list_number(state) {
-    return state.list_number
+    return state.list_number;
   },
   isLogin(state) {
-    return state.data !== null
-  },
-}
+    return state.data !== null;
+  }
+};
 // ______________________________________________________
 //
 export const mutations: Mutations<S, M> = {
-  /**
-   * 時間割の上書き更新
-   * @param payload module: 更新する学期, data: その学期の時間割情報すべて
-   */
   updateTable(state, payload) {
     if (state.data === null) {
-      state.data = new Array(6)
+      state.data = new Array(6);
     }
-    const list: string[] = ['haruA', 'haruB', 'haruC', 'akiA', 'akiB', 'akiC']
-    const position: number = list.indexOf(payload.module)
-    state.data[position] = payload.data // -1の状態だめじゃん
-    localStorage.setItem('table', JSON.stringify(state.data))
+    const position: number = state.moduleList.indexOf(payload.module);
+    state.data[position] = payload.data; // -1の状態だめじゃん
+    localStorage.setItem("table", JSON.stringify(state.data));
   },
   updateTableAll(state, payload) {
     if (payload.data === null) {
-      state.data = null
-      localStorage.removeItem('table')
-      return
+      state.data = null;
+      localStorage.removeItem("table");
+      return;
     }
-    state.data = payload.data
-    localStorage.setItem('table', JSON.stringify(state.data))
+    state.data = payload.data;
+    localStorage.setItem("table", JSON.stringify(state.data));
   },
-  /**
-   * 授業番号のリストに重複なく追加+nullチェック
-   * @param payload module: 追加する学期 ex) "haruA", data: 授業番号
-   */
-  pushNumber(state, payload) {
-    const list: string[] = ['haruA', 'haruB', 'haruC', 'akiA', 'akiB', 'akiC']
-    const position: number = list.indexOf(payload.module)
+  SET_NUMBER(state, payload) {
+    const position: number = state.moduleList.indexOf(payload.module);
     state.list_number[position] = union(
       state.list_number[position],
       payload.data
-    )
-    localStorage.setItem('number', JSON.stringify(state.list_number))
+    );
+    localStorage.setItem("number", JSON.stringify(state.list_number));
   },
-  pushNumberAll(state, payload) {
+  SET_NUMBERS(state, payload) {
     if (payload.data === null) {
-      state.list_number = []
-      localStorage.removeItem('number')
-      return
+      state.list_number = [];
+      localStorage.removeItem("number");
+      return;
     }
-    state.list_number = payload.data
-    localStorage.setItem('number', JSON.stringify(state.list_number))
-  },
-}
+    state.list_number = payload.data;
+    localStorage.setItem("number", JSON.stringify(state.list_number));
+  }
+};
 // ______________________________________________________
 //
 export const actions: Actions<S, A, G, M> = {
   async asyncNumber(ctx, payload) {
+    ctx.dispatch("createNumbers");
     const list =
-      ctx.state.list_number[ctx.state.moduleList.indexOf(payload.module)]
-    const numbers = union(list, payload.number)
+      ctx.state.list_number[ctx.state.moduleList.indexOf(payload.module)];
+    const numbers = union(list, payload.number);
     try {
       await axios
-        .post('https://twinte.net/api', {
+        .post("https://twinte.net/api", {
           number: numbers,
-          view_season: payload.module,
+          view_season: payload.module
         })
-        .then((items) => {
-          if (items.data[0] === null) return // 違うデータだった場合は追加しない
-          ctx.commit('updateTable', {
+        .then(items => {
+          if (items.data[0] === null) return; // 違うデータだった場合は追加しない
+          ctx.commit("updateTable", {
             module: payload.module,
-            data: items.data,
-          })
-          ctx.commit('pushNumber', {
-            module: payload.module,
-            data: payload.number,
-          })
-        })
+            data: items.data
+          });
+          ctx.dispatch("assignObj", {
+            moduleNum: ctx.state.moduleList.indexOf(payload.module)
+          });
+          ctx.dispatch("createNumbers");
+          window.alert("時間割を追加しました");
+        });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   },
   async asyncCSV(ctx, payload) {
-    ctx.commit('pushNumberAll', { data: [] })
+    ctx.commit("SET_NUMBER", { module: payload.module, data: [] });
     await axios
-      .post('https://twinte.net/api', payload.formData, payload.config)
+      .post("https://twinte.net/api", payload.formData, payload.config)
       .then(function(items) {
-        ctx.commit('updateTable', { module: payload.module, data: items.data })
+        ctx.commit("updateTable", { module: payload.module, data: items.data });
+        ctx.dispatch("assignObj", {
+          moduleNum: ctx.state.moduleList.indexOf(payload.module)
+        });
+        ctx.dispatch("createNumbers");
+        window.alert("時間割を追加しました");
       })
       .catch(function(err) {
-        console.error(err)
-      })
+        console.error(err);
+      });
   },
   assignObj(ctx, payload) {
-    const source = { attend: 0, absent: 0, memo: '' }
-    const data = ctx.getters.data
+    const source = { attend: 0, absent: 0, late: 0, memo: "" };
+    const data = ctx.getters.data;
     if (data === null) {
-      return
+      return;
     }
-    data[payload.moduleNum].map((a) => {
-      return a.map((b) => {
-        return Object.assign(b, source)
+    if (data[payload.moduleNum] === undefined) {
+      return;
+    }
+    data[payload.moduleNum].map(a => {
+      return a.map(b => {
+        return b.absent === null ? Object.assign(b, source) : b;
+      });
+    });
+    console.dir(data);
+    ctx.commit("updateTableAll", { data: data });
+  },
+  createNumbers(ctx) {
+    const table = ctx.getters["data"];
+    if (table === null) {
+      return;
+    }
+
+    const data = new Array(6);
+    table.forEach((a, i) => {
+      a.forEach(b => {
+        b.forEach(c => {
+          if (c.number !== '') {
+            data[i] = union(data[i], [c.number]);
+          }
+        })
       })
-    })
-    console.dir(data)
-    ctx.commit('updateTableAll', { data: data })
+    });
+    
+    ctx.commit("SET_NUMBERS", { data });
+    console.log(data);
+    
   },
   login(ctx) {
-    ctx.commit('updateTableAll', { data: tableData })
+    ctx.commit("updateTableAll", { data: tableData });
   },
   logout(ctx) {
-    ctx.commit('updateTableAll', { data: null })
-    ctx.commit('pushNumberAll', { data: null })
-  },
-}
+    ctx.commit("updateTableAll", { data: null });
+    ctx.commit("SET_NUMBERS", { data: null });
+  }
+};
--- src/store/old_api/type.ts
+++ src/store/old_api/type.ts
@@ -1,49 +1,60 @@
 export interface Table {
-  classroom: string
-  name: string
-  number: string
-  season: string
-  teacher: string
-  time: string
-  attend: number
-  absent: number
-  memo: string
+  classroom: string;
+  name: string;
+  number: string;
+  season: string;
+  teacher: string;
+  time: string;
+  attend: number;
+  absent: number;
+  late: number;
+  memo: string;
 }
-;[]
-export type ResponseData = Table[][]
+export type ResponseData = Table[][];
 
 // state______________________________________________________
 //
 export interface S {
-  data: Table[][][] | null
-  list_number: string[][]
-  moduleList: string[]
+  data: Table[][][] | null;
+  list_number: string[][];
+  moduleList: string[];
 }
 // getters______________________________________________________
 //
 export interface G {
-  data: Table[][][] | null
-  list_number: string[][] | null
-  isLogin: boolean
+  data: Table[][][] | null;
+  list_number: string[][] | null;
+  isLogin: boolean;
 }
 export interface RG {
-  'old_api/data': G['data']
-  'old_api/list_number': G['list_number']
-  'old_api/isLogin': G['isLogin']
+  "old_api/data": G["data"];
+  "old_api/list_number": G["list_number"];
+  "old_api/isLogin": G["isLogin"];
 }
-// mutations
+// mutations______________________________________________________
 //
 export interface M {
-  updateTable: { module: string; data: Table[][] }
-  pushNumber: { module: string; data: string[] }
-  updateTableAll: { data: Table[][][] | null }
-  pushNumberAll: { data: string[][] | null }
+  /**
+   * 時間割の上書き更新
+   * @param payload module: 更新する学期 "haruA", data: その学期の時間割情報すべて
+   */
+  updateTable: { module: string; data: Table[][] };
+  updateTableAll: { data: Table[][][] | null };
+  /**
+   * 授業番号のリストに重複なく追加+nullチェック
+   * @param payload module: 追加する学期 ex) "haruA", data: 授業番号
+   */
+  SET_NUMBER: { module: string; data: string[] };
+  /**
+   * すべての学期の授業番号を変更するため`data`配列は[module][number配列]の形にする
+   */
+  SET_NUMBERS: { data: string[][] | null };
 }
 export interface RM {
-  'old_api/updateTable': M['updateTable']
-  'old_api/pushNumber': M['pushNumber']
-  'old_api/updateTableAll': M['updateTableAll']
-  'old_api/pushNumberAll': M['pushNumberAll']
+  "old_api/updateTable": M["updateTable"];
+  "old_api/SET_NUMBER": M["SET_NUMBER"];
+  "old_api/updateTableAll": M["updateTableAll"];
+  "old_api/SET_NUMBERS": M["SET_NUMBERS"];
 }
 // actions______________________________________________________
 //
@@ -52,24 +63,29 @@ export interface A {
    * 番号での追加
    * @param module api基準 "haruA"
    */
-  asyncNumber: { number: string[]; module: string }
+  asyncNumber: { number: string[]; module: string };
   /**
    * CSVでの追加
    * @param module api基準 "haruA"
    */
-  asyncCSV: { formData: any; config: any; module: string }
+  asyncCSV: { formData: any; config: any; module: string };
   /**
    * moduleNum学期に
    * attend: 0, absent: 0, memo: '' を追加
    */
-  assignObj: { moduleNum: number }
-  login: null
-  logout: null
+  assignObj: { moduleNum: number };
+  /**
+   * 授業リストの作成
+   */
+  createNumbers: null;
+  login: null;
+  logout: null;
 }
 export interface RA {
-  'old_api/asyncNumber': A['asyncNumber']
-  'old_api/asyncCSV': A['asyncCSV']
-  'old_api/assignObj': A['assignObj']
-  'old_api/login': A['login']
-  'old_api/logout': A['logout']
+  "old_api/asyncNumber": A["asyncNumber"];
+  "old_api/asyncCSV": A["asyncCSV"];
+  "old_api/assignObj": A["assignObj"];
+  "old_api/createNumbers": A["createNumbers"];
+  "old_api/login": A["login"];
+  "old_api/logout": A["logout"];
 }
