4aa19425c4d34a893d240c8a1023267e09f4007c
HikaruEgashira <shighten@gmail.com>
2019-10-23 14:24:06 +0900

    init for dev

---
+++ src/pages/table.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <app-time-table-header />
+    <app-time-table />
+    <app-dialog />
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component({
+  components: {
+    AppTimeTableHeader: () => import("~/components/app-table-header.vue"),
+    AppTimeTable: () => import("~/components/app-table-contents.vue"),
+    AppDialog: () => import("~/components/app-dialog-detail.vue")
+  }
+})
+export default class Index extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
