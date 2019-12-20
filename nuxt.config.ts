8b36ddfa8085ade554fb961477fd760c6ad81c27
HikaruEgashira <shighten@gmail.com>
2019-12-20 11:49:59 +0900

    add ogp image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,98 +1,103 @@
-import { Configuration } from "@nuxt/types";
-const baseName = process.env.BASE_NAME || "Twin:te";
+import { Configuration } from '@nuxt/types'
+const baseName = process.env.BASE_NAME || 'Twin:te'
 const baseDesc =
-  process.env.BASE_DISC || "筑波大学専門の時間割アプリケーションです";
-const baseUrl = process.env.BASE_URL || "http://localhost:3000";
-const baseDir = process.env.BASE_DIR || "/";
+  process.env.BASE_DISC || '筑波大学専門の時間割アプリケーションです'
+const baseUrl = process.env.BASE_URL || 'http://localhost:3000'
+const baseDir = process.env.BASE_DIR || '/'
 
 const nuxtConfig: Configuration = {
-  buildModules: ["@nuxt/typescript-build"],
-  mode: "universal",
-  srcDir: "src/",
+  buildModules: ['@nuxt/typescript-build'],
+  mode: 'universal',
+  srcDir: 'src/',
   env: {
     baseName,
     baseDesc,
-    baseUrl
+    baseUrl,
   },
   router: {
-    base: baseDir
+    base: baseDir,
   },
   server: {
     port: process.env.PORT || 5000,
-    host: "localhost"
+    host: 'localhost',
   },
   head: {
     htmlAttrs: {
-      lang: "ja",
-      prefix: "og: http://ogp.me/ns#"
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#',
     },
     title: baseName,
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: baseDesc },
-      { hid: "og:site_name", property: "og:site_name", content: baseName },
-      { hid: "og:type", property: "og:type", content: "article" },
-      { hid: "og:url", property: "og:url", content: baseUrl },
-      { hid: "og:title", property: "og:title", content: baseName },
-      { hid: "og:description", property: "og:description", content: baseDesc },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: baseDesc },
+      { hid: 'og:site_name', property: 'og:site_name', content: baseName },
+      { hid: 'og:type', property: 'og:type', content: 'summary_large_image' },
+      { hid: 'og:url', property: 'og:url', content: baseUrl },
+      { hid: 'og:title', property: 'og:title', content: baseName },
       {
-        hid: "og:image",
-        property: "og:image",
-        content: `${baseUrl}/icon.png`
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://www.twinte.net/ogp.png',
       },
-      { name: "twitter:card", content: baseDesc },
-      { name: "twitter:site", content: "@te_twin" }
-    ]
+      { hid: 'og:description', property: 'og:description', content: baseDesc },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: `${baseUrl}/icon.png`,
+      },
+      { name: 'twitter:card', content: baseDesc },
+      { name: 'twitter:site', content: '@te_twin' },
+    ],
   },
   loadingIndicator: {
-    name: "wandering-cubes",
-    color: "teal",
-    color2: "white",
-    background: "gray"
+    name: 'wandering-cubes',
+    color: 'teal',
+    color2: 'white',
+    background: 'gray',
   },
   loading: {
-    color: "teal",
-    height: "5px"
+    color: 'teal',
+    height: '5px',
   },
-  css: ["@/assets/css/main.scss"],
-  modules: ["@nuxtjs/pwa"],
-  plugins: [{ src: "@/plugins/ga.js", mode: "client" }],
+  css: ['@/assets/css/main.scss'],
+  modules: ['@nuxtjs/pwa'],
+  plugins: [{ src: '@/plugins/ga.js', mode: 'client' }],
   workbox: {
     dev: false,
     runtimeCaching: [
       {
-        urlPattern: "^https://fonts.(?:googleapis|gstatic).com/(.*)",
-        handler: "cacheFirst"
+        urlPattern: '^https://fonts.(?:googleapis|gstatic).com/(.*)',
+        handler: 'cacheFirst',
       },
       {
-        urlPattern: baseDir + "lib/.*",
-        handler: "cacheFirst",
+        urlPattern: baseDir + 'lib/.*',
+        handler: 'cacheFirst',
         strategyOptions: {
-          cacheName: "image-cache",
+          cacheName: 'image-cache',
           cacheExpiration: {
             maxEntries: 20,
-            maxAgeSeconds: 24 * 60 * 60 * 30
-          }
-        }
-      }
-    ]
+            maxAgeSeconds: 24 * 60 * 60 * 30,
+          },
+        },
+      },
+    ],
   },
   build: {
-    analyze: false
+    analyze: false,
   },
   meta: {
     mobileAppIOS: true,
     appleStatusBarStyle: false,
-    lang: "ja",
-    nativeUI: true
+    lang: 'ja',
+    nativeUI: true,
   },
   manifest: {
-    name: "Twin:te",
-    short_name: "Twin:te",
-    lang: "ja",
-    start_url: "/",
-    display: "standalone"
+    name: 'Twin:te',
+    short_name: 'Twin:te',
+    lang: 'ja',
+    start_url: '/',
+    display: 'standalone',
   },
   features: {
     store: true,
@@ -108,8 +113,8 @@ const nuxtConfig: Configuration = {
     clientPrefetch: true,
     clientUseUrl: true,
     componentAliases: true,
-    componentClientOnly: true
-  }
-};
+    componentClientOnly: true,
+  },
+}
 
-module.exports = nuxtConfig;
+module.exports = nuxtConfig
