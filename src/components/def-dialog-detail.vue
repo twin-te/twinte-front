2055e8ff6091ddff6a8a5f8869f49424ace62864
HikaruEgashira <shighten@gmail.com>
2019-12-28 13:59:41 +0900

    Fix 別タブで開くようにした

--- src/components/def-dialog-detail.vue
+++ src/components/def-dialog-detail.vue
@@ -130,11 +130,20 @@ export default class Index extends Vue {
 
   syllabus() {
     if (this.table) {
-      switch(this.table.lecture_code.split('').splice(0, 2).join("")) {
+      switch (
+        this.table.lecture_code
+          .split('')
+          .splice(0, 2)
+          .join('')
+      ) {
         case 'GB':
-          location.href = `http://www.coins.tsukuba.ac.jp/syllabus/${this.table.lecture_code}.html`;
+          window.open(
+            `http://www.coins.tsukuba.ac.jp/syllabus/${this.table.lecture_code}.html`
+          );
         default:
-          location.href = `https://kdb.tsukuba.ac.jp/syllabi/2019/${this.table.lecture_code}/jpn/#course-title`;
+          window.open(
+            `https://kdb.tsukuba.ac.jp/syllabi/2019/${this.table.lecture_code}/jpn/#course-title`
+          );
       }
     }
   }
