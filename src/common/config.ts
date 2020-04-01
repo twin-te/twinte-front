4181c2563532ceab53050a359ed97d795e882a24
HikaruEgashira <shighten@gmail.com>
2020-04-01 23:00:07 +0900

    getMonth()に+1を追加

---
+++ src/common/config.ts
@@ -0,0 +1,10 @@
+export const BASE_URL: string =
+  process.env.API_URL || 'https://api.twinte.net/v1';
+const date = new Date();
+export const YEAR: number =
+  date.getMonth() + 1 < 4 ? date.getFullYear() - 1 : date.getFullYear();
+
+import _axios from 'axios';
+export const axios = _axios.create({
+  withCredentials: true
+});
