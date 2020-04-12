684998a055055ef6d49e36c6efa7f8660966b67c
HikaruEgashira <shighten@gmail.com>
2020-04-12 14:55:47 +0900

    BEM like

--- src/components/app-table-header.vue
+++ src/components/app-table-header.vue
@@ -1,17 +1,21 @@
 <template>
-  <section>
-    <div id="module">
-      <span class="s4 material-icons svg-button" @click="prevModule"
+  <section class="table-header">
+    <div class="table-header__module">
+      <span
+        class="table-header__icon material-icons svg-button"
+        @click="prevModule"
         >chevron_left</span
       >
       <span>{{ module }}</span>
-      <span class="s4 material-icons svg-button" @click="nextModule"
+      <span
+        class="table-header__icon material-icons svg-button"
+        @click="nextModule"
         >chevron_right</span
       >
     </div>
-    <div id="week">
-      <day id="day"></day>
-      <section class="week-wrapper">
+    <div class="table-header__week">
+      <day class="table-header__week-cal"></day>
+      <section class="table-header__week-wrapper">
         <div v-for="n in 5" :key="n">{{ week[n - 1] }}</div>
       </section>
     </div>
@@ -69,44 +73,48 @@ export default class Index extends Vue {
 $week-width: calc(
   100vw - 8vw /*contentのmargin+padding*/ - 13vw /*timeのwidth+padding*/
 );
-.s4 {
-  display: block;
-  width: 15vw;
-  font-size: 4.6vh;
-  text-align: center;
-}
-#module {
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  font-family: Noto Sans JP;
-  color: #9a9a9a;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 2.2vh;
-  line-height: 4.6vh;
-  width: 45vw;
-  top: 0.5vh;
-  left: 50%;
-  transform: translateX(-50%);
-}
-#week {
-  position: relative;
-  display: flex;
-  width: 87vw;
-  height: 2.5vh;
-  font-family: Noto Sans JP;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 2vh;
-  color: #9a9a9a;
-}
-.week-wrapper {
-  position: absolute;
-  display: flex;
-  width: $week-width;
-  justify-content: space-around;
-  left: calc(13vw + 4vw);
-  top: 0.6vh;
+
+.table-header {
+  &__icon {
+    display: block;
+    width: 15vw;
+    font-size: 4.6vh;
+    text-align: center;
+  }
+  &__module {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    font-family: Noto Sans JP;
+    color: #9a9a9a;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 2.2vh;
+    line-height: 4.6vh;
+    width: 45vw;
+    top: 0.5vh;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  &__week {
+    position: relative;
+    display: flex;
+    width: 87vw;
+    height: 2.5vh;
+    font-family: Noto Sans JP;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 2vh;
+    color: #9a9a9a;
+
+    &-wrapper {
+      position: absolute;
+      display: flex;
+      width: $week-width;
+      justify-content: space-around;
+      left: calc(13vw + 4vw);
+      top: 0.6vh;
+    }
+  }
 }
 </style>
