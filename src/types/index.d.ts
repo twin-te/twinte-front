7275ff2d02d311f46ae53ea3a7856cf5d13cf968
HikaruEgashira <shighten@gmail.com>
2020-04-24 00:11:04 +0900

    型更新

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -10,18 +10,22 @@ type TimeTables = OutputPeriodData[]
 type Period = OutputPeriodData
 
 type OutputSearchData = {
-  twinte_lecture_id: string
-  year: number
   lectureCode: string
   name: string
+  credits: number
+  overview: string
+  remarks: string
+  type: number
   details: {
-    id: number
     module: Module
     day: Day
     period: number
     room: string
   }[]
   instructor: string
+  twinte_lecture_id: string
+  year: number
+  standardYear: number[]
 }[]
 
 type SubjectSettings = {
