78455c2ead6fa8d4a0f03de0569921757048cf46
HikaruEgashira <shighten@gmail.com>
2020-04-25 19:43:57 +0900

    バリデーションを追加

--- src/components/utils/addCustomLecture.ts
+++ src/components/utils/addCustomLecture.ts
@@ -46,92 +46,89 @@ const periodOption = {
   6: '6',
 }
 
-const view = async (): Promise<Form | null> => {
-  let form: Form = {
-    lecture_name: '',
-    instructor: '',
-    module: '',
-    day: '',
-    period: '',
-    room: '',
-  }
+const initFormValue = {
+  lecture_name: '',
+  instructor: '',
+  module: '',
+  day: '',
+  period: '',
+  room: '',
+}
+
+const validator = (form: Form): boolean => {
+  return form.module !== '' && form.day !== '' && form.period !== ''
+}
+
+const makeForm = async (): Promise<Form> => {
+  let form: Form = initFormValue
 
   await Swal.mixin({
     confirmButtonText: '次へ &rarr;',
     showCancelButton: true,
     progressSteps: ['1', '2', '3', '4', '5', '6'],
-  })
-    .queue([
-      {
-        title: 'カスタム授業の作成',
-        text: '科目名を入力してください。',
-        input: 'text',
-        preConfirm: (lecture_name: string) => {
-          form.lecture_name = lecture_name
-        },
+  }).queue([
+    {
+      title: 'カスタム授業の作成',
+      text: '科目名を入力してください。',
+      input: 'text',
+      preConfirm: (lecture_name: string) => {
+        form.lecture_name = lecture_name || ''
       },
-      {
-        title: 'カスタム授業の作成',
-        text: '教師名を入力してください。',
-        input: 'text',
-        preConfirm: (instructor: string) => {
-          form.instructor = instructor
-        },
+    },
+    {
+      title: 'カスタム授業の作成',
+      text: '教師名を入力してください。',
+      input: 'text',
+      preConfirm: (instructor: string) => {
+        form.instructor = instructor || ''
       },
-      {
-        title: 'カスタム授業の作成',
-        text:
-          'モジュールを選択してください。複数のモジュールの追加には現在対応していません。',
-        input: 'select',
-        inputOptions: moduleOption,
-        inputPlaceholder: 'クリックしてモジュールを選択する',
-        preConfirm: (module: keyof typeof moduleOption) => {
-          form.module = moduleOption[module]
-        },
+    },
+    {
+      title: 'カスタム授業の作成',
+      text: '場所を入力してください。',
+      input: 'text',
+      preConfirm: (room: string) => {
+        form.room = room || ''
       },
-      {
-        title: 'カスタム授業の作成',
-        text: '曜日を選択してください。',
-        input: 'select',
-        inputOptions: dayOption,
-        inputPlaceholder: 'クリックして曜日を選択する',
-        preConfirm: (day: keyof typeof dayOption) => {
-          form.day = dayOption[day]
-        },
+    },
+    {
+      title: 'カスタム授業の作成',
+      text:
+        'モジュールを選択してください。複数のモジュールの追加には現在対応していません。',
+      input: 'select',
+      inputOptions: moduleOption,
+      inputPlaceholder: 'クリックしてモジュールを選択する',
+      preConfirm: (module: keyof typeof moduleOption) => {
+        form.module = moduleOption[module] || ''
       },
-      {
-        title: 'カスタム授業の作成',
-        text: '時限を選択してください。',
-        input: 'radio',
-        inputOptions: periodOption,
-        preConfirm: (period: string) => {
-          form.period = period
-        },
+    },
+    {
+      title: 'カスタム授業の作成',
+      text: '曜日を選択してください。',
+      input: 'select',
+      inputOptions: dayOption,
+      inputPlaceholder: 'クリックして曜日を選択する',
+      preConfirm: (day: keyof typeof dayOption) => {
+        form.day = dayOption[day] || ''
       },
-      {
-        title: 'カスタム授業の作成',
-        text: '場所を入力してください。',
-        input: 'text',
-        preConfirm: (room: string) => {
-          form.room = room
-        },
+    },
+    {
+      title: 'カスタム授業の作成',
+      text: '時限を選択してください。',
+      input: 'radio',
+      inputOptions: periodOption,
+      preConfirm: (period: string) => {
+        form.period = period || ''
       },
-    ])
-    .then((result) => {
-      if (result.value) {
-        return form
-      } else {
-        Swal.fire('追加をキャンセルしました', '', 'info')
-        return null
-      }
-    })
+    },
+  ])
 
   return form
 }
 
 export const addCustomLecture = async () => {
-  const form = await view()
-  if (form) {
+  const form = await makeForm()
+  if (validator(form)) {
     // 時間割の作成
     const userData = await postUserData(form.lecture_name, form.instructor)
     if (!userData) {
@@ -154,6 +151,10 @@ export const addCustomLecture = async () => {
     await updateLecture(lecture)
     Swal.fire('追加が完了しました', '', 'success')
   } else {
-    Swal.fire('追加をキャンセルしました。', '', 'info')
+    Swal.fire(
+      '追加をキャンセルしました。',
+      '時間割追加がうまく行えなかった可能性があります。',
+      'info'
+    )
   }
 }
