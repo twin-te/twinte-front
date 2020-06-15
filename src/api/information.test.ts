28621f5c9a8be261d6dc3a5ee3a462c98c272a58
HikaruEgashira <shighten@gmail.com>
2020-06-15 23:12:39 +0900

    Add snap shot

--- src/api/information.test.ts
+++ src/api/information.test.ts
@@ -1,10 +1,19 @@
-import { getInformation } from './information'
+import { getInformation, parse } from './information'
 
 jest.setTimeout(30000)
 
-describe(getInformation, () => {
+describe(getInformation.name, () => {
   it('basic usage', async () => {
     const info = await getInformation()
     expect(info.status).toBe(200)
+    expect(info.data.slice(-1)[0]).toMatchSnapshot('info__result')
+  })
+})
+
+describe(parse.name, () => {
+  it('parse is Endomophism', async () => {
+    const info = await getInformation()
+    const parsedInfo = parse(info)
+    expect(parsedInfo.slice(-1)[0]).toMatchSnapshot('info__result__parced')
   })
 })
