3d39697dc5d01e31c436d2b044de1ee363e909d2
HikaruEgashira <shighten@gmail.com>
2020-11-09 15:17:00 +0900

    APIの環境変数を使いやすくした

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,7 @@
-export const BASE_URL: string =
-  process.env.API_URL || 'https://api.twinte.net/v1'
+export const BASE_URL =
+  process.env.NODE_ENV != 'production'
+    ? 'https://api.twinte.net/v1' // 'https://api.dev.twinte.net/v1'
+    : process.env.API_URL || 'https://api.twinte.net/v1'
 const date = new Date()
-export const YEAR: number =
+export const YEAR =
   date.getMonth() + 1 < 4 ? date.getFullYear() - 1 : date.getFullYear()
--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -1,7 +1,8 @@
 import { Plugin } from '@nuxt/types'
 import axiosClient from '@aspida/axios'
-import api, { ApiInstance } from '~/api/$api'
 import { AxiosInstance } from 'axios'
+import api, { ApiInstance } from '~/api/$api'
+import { BASE_URL } from '~/config'
 
 declare module 'vue/types/vue' {
   interface Vue {
@@ -12,7 +13,12 @@ declare module 'vue/types/vue' {
 const plugin: Plugin = ({ $axios }, inject) =>
   inject(
     'api',
-    api(axiosClient($axios as AxiosInstance, { withCredentials: true }))
+    api(
+      axiosClient($axios as AxiosInstance, {
+        withCredentials: true,
+        baseURL: BASE_URL,
+      })
+    )
   )
 
 export default plugin
