45d677bd980143d5119f83c22cd1a5c078754a8a
HosokawaRyunosuke <thin.river.jp@gmail.com>
2021-01-21 07:43:59 +0900

    icon button を追加

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <Button @click="clickHandler" size="small">Button</Button>
+  <Icon-Button @click="clickHandler" size="small" color="nomal" iconName="expand_more"></Icon-Button>
+  <Icon-Button @click="clickHandler" size="medium" color="nomal" iconName="edit"></Icon-Button>
+  <Icon-Button @click="clickHandler" size="large" color="nomal" iconName="menu"></Icon-Button>
+  <Icon-Button @click="clickHandler" size="large" color="danger" iconName="delete"></Icon-Button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Button from "./components/Button.vue";
+import IconButton from "./components/IconButton.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     Button,
+    IconButton
   },
   setup: () => {
     const clickHandler = () => {
