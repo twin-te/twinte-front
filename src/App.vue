c567bb1598e8ca21bfd0efd0062309868cedf482
HosokawaRyunosuke <thin.river.jp@gmail.com>
2021-01-25 20:03:23 +0900

    SignleLineTextField を追加

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,46 @@
 <template>
   <Button @click="clickHandler" size="small">Button</Button>
+  <TextFieldSingleLine
+    v-model="text1"
+    @enter-text-field="enterHandler(text1)"
+    placeholder="e.g."
+    iconName="search"
+  ></TextFieldSingleLine>
+  {{ text1 }}
+  <TextFieldSingleLine
+    v-model="text2"
+    @enter-text-field="enterHandler(text2)"
+    placeholder="例）ゼミ"
+    :width="20"
+    :height="3"
+  ></TextFieldSingleLine>
+  {{ text2 }}
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import Button from "./components/Button.vue";
+import TextFieldSingleLine from "./components/TextFieldSingleLine.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     Button,
+    TextFieldSingleLine,
   },
   setup: () => {
+    const text1 = ref("");
+    const text2 = ref("");
+
     const clickHandler = () => {
       console.log("click");
     };
 
-    return { clickHandler };
+    const enterHandler = (text: string) => {
+      console.log('enter ! ', text);
+    };
+
+    return { clickHandler, enterHandler, text1, text2 };
   },
 });
 </script>
