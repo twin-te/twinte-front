51046cfebe7e48afa67d07e9fd50377cc22d35c8
HikaruEgashira <shighten@gmail.com>
2021-01-26 10:41:06 +0900

    feat(dependencies): add router
    vue-routerを追加した。
    addページを作成

---
+++ src/pages/add/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <Button @click="clickHandler" size="small">Back to Home</Button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+import Button from "../../components/Button.vue";
+
+export default defineComponent({
+  name: "Add",
+  components: {
+    Button,
+  },
+  setup: () => {
+    const router = useRouter();
+
+    const clickHandler = async () => {
+      await router.push("/");
+    };
+
+    return { clickHandler };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
