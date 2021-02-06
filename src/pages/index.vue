e7d4e4ddfce62cbb8246f68acde022eaf8713e59
HosokawaRyunosuke <thin.river.jp@gmail.com>
2021-02-06 21:03:36 +0900

    CourseTile Component を作成

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -31,6 +31,27 @@
     color="danger"
     icon-name="delete"
   ></IconButton>
+
+  <div class="course-grid">
+    <CourseTile
+      @click="tileStat = tileStat == 'active' ? 'default' : 'active'"
+      :state="tileStat"
+      course-name="学校を考える"
+      course-id="1A101"
+    ></CourseTile>
+    <CourseTile
+      state="active"
+      course-name="学校を考える"
+      course-id="1A101"
+    ></CourseTile>
+    <CourseTile state="none"></CourseTile>
+    <CourseTile
+      state="default"
+      course-name="学校を考える"
+      course-id="1A101"
+      caution="他1件"
+    ></CourseTile>
+  </div>
 </template>
 
 <script lang="ts">
@@ -38,6 +59,7 @@ import { defineComponent, ref } from "vue";
 import Button from "../components/Button.vue";
 import IconButton from "../components/IconButton.vue";
 import ToggleIconButton from "../components/ToggleIconButton.vue";
+import CourseTile from "../components/CourseTile.vue";
 
 export default defineComponent({
   name: "App",
@@ -45,6 +67,7 @@ export default defineComponent({
     Button,
     IconButton,
     ToggleIconButton,
+    CourseTile,
   },
   setup: () => {
     const isBtnActive = ref({
@@ -53,13 +76,24 @@ export default defineComponent({
       menu: false,
     });
 
+    const tileStat = ref("default" as "default" | "active" | "none");
+
     const clickHandler = () => {
       console.log("click");
     };
 
-    return { clickHandler, isBtnActive };
+    return { clickHandler, isBtnActive, tileStat };
   },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.course-grid {
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.2rem;
+  width: 220px;
+  height: 300px;
+}
+</style>
