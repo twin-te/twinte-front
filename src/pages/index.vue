67ca26f1ce42f0593b0865c9785aa53f18f8e41b
HosokawaR <45098934+HosokawaR@users.noreply.github.com>
2021-02-07 17:52:00 +0900

    Revert "CourseTile と CourseDetail を追加"

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -34,52 +34,6 @@
     color="danger"
     icon-name="delete"
   ></IconButton>
-
-  <div class="course-grid">
-    <CourseTile
-      @click="tileStat = tileStat == 'active' ? 'default' : 'active'"
-      :state="tileStat"
-      courseName="学校を考える"
-      courseId="1A101"
-    ></CourseTile>
-
-    <CourseTile
-      state="active"
-      courseName="学校を考える"
-      courseId="1A101"
-    ></CourseTile>
-
-    <CourseTile state="none" courseName="" courseId=""></CourseTile>
-
-    <CourseTile
-      state="default"
-      courseName="学校を考える"
-      courseId="1A101"
-      caution="他1件"
-    ></CourseTile>
-  </div>
-
-  <div class="course-details">
-    <CourseDetail
-      iconName="schedule"
-      item="開講時限"
-      value="春AB 木2"
-    ></CourseDetail>
-
-    <CourseDetail
-      iconName="person"
-      item="担当教員"
-      value="山本早里"
-    ></CourseDetail>
-
-    <CourseDetail iconName="room" item="授業場所" value="6A508"></CourseDetail>
-
-    <CourseDetail
-      iconName="category"
-      item="授業形式"
-      value="対面"
-    ></CourseDetail>
-  </div>
 </template>
 
 <script lang="ts">
@@ -89,10 +43,6 @@ import { authCheck } from "~/usecases/authCheck";
 import Button from "../components/Button.vue";
 import IconButton from "../components/IconButton.vue";
 import ToggleIconButton from "../components/ToggleIconButton.vue";
-import CourseTile, {
-  State as CourseTileState,
-} from "../components/CourseTile.vue";
-import CourseDetail from "../components/CourseDetail.vue";
 
 export default defineComponent({
   name: "App",
@@ -100,8 +50,6 @@ export default defineComponent({
     Button,
     IconButton,
     ToggleIconButton,
-    CourseTile,
-    CourseDetail,
   },
   setup: () => {
     const { ready, state } = useUsecase(authCheck, true);
@@ -111,30 +59,13 @@ export default defineComponent({
       menu: false,
     });
 
-    const tileStat = ref<CourseTileState>("default");
-
     const clickHandler = () => {
       console.log("click");
     };
 
-    return { clickHandler, isBtnActive, ready, state, tileStat };
+    return { clickHandler, isBtnActive, ready, state };
   },
 });
 </script>
 
-<style lang="scss" scoped>
-.course-grid {
-  display: grid;
-  grid-template-rows: repeat(2, 1fr);
-  grid-template-columns: repeat(2, 1fr);
-  gap: 0.2rem;
-  width: 220px;
-  height: 300px;
-}
-
-.course-details {
-  display: flex;
-  flex-flow: column;
-  gap: 1rem;
-}
-</style>
+<style lang="scss" scoped></style>
