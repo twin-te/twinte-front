e954480387f6e8012cefd0c56750a465ee5cece0
HosokawaRyunosuke <thin.river.jp@gmail.com>
2021-03-02 05:22:02 +0900

    outside click event を付加

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import App from "./App.vue";
 import { router } from "./route";
 import { store } from "./store";
 import "./scss/main.scss";
+import VueClickAway from "vue3-click-away";
 
-createApp(App).use(router).use(store).mount("#app");
+createApp(App).use(router).use(store).use(VueClickAway).mount("#app");
