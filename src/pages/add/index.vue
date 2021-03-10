dd9ff872b1ebf0d81a6457b228a1a1ad14ce1e6d
HosokawaRyunosuke <thin.river.jp@gmail.com>
2021-03-11 03:33:07 +0900

    css を修正

--- src/pages/add/index.vue
+++ src/pages/add/index.vue
@@ -2,40 +2,50 @@
   <div class="add">
     <PageHeader>
       <template #left-btn>
-        <IconButton
-          @click="toggleSidebar"
-          size="large"
-          color="normal"
-          iconName="menu"
-        ></IconButton
-      ></template>
+        <div class="header__left-btn">
+          <IconButton
+            @click="toggleSidebar"
+            size="large"
+            color="normal"
+            iconName="menu"
+          ></IconButton>
+        </div>
+      </template>
       <template #title>授業の追加</template>
     </PageHeader>
     <div class="main">
-      <CardAdd
-        @click-next-button="moveTo('/')"
-        iconName="search"
-        heading="授業の検索"
-        text="ワードや条件を指定して授業を検索・追加します。"
-      ></CardAdd>
-      <CardAdd
-        @click-next-button="moveTo('/')"
-        iconName="get_app"
-        heading="Twinsからインポート"
-        text="Twinsで登録した授業をモジュール単位でインポートします。"
-      ></CardAdd>
-      <CardAdd
-        @click-next-button="moveTo('/')"
-        iconName="insert_drive_file"
-        heading="CSVファイルから追加"
-        text="CSVファイルに入っている授業データを追加します。"
-      ></CardAdd>
-      <CardAdd
-        @click-next-button="moveTo('/')"
-        iconName="style"
-        heading="手動で授業を作成"
-        text="手入力でカスタマイズして授業を作成します。"
-      ></CardAdd>
+      <div class="main__card">
+        <CardAdd
+          @click-next-button="$router.push('/')"
+          iconName="search"
+          heading="授業の検索"
+          text="ワードや条件を指定して授業を検索・追加します。"
+        ></CardAdd>
+      </div>
+      <div class="main__card">
+        <CardAdd
+          @click-next-button="$router.push('/')"
+          iconName="get_app"
+          heading="Twinsからインポート"
+          text="Twinsで登録した授業をモジュール単位でインポートします。"
+        ></CardAdd>
+      </div>
+      <div class="main__card">
+        <CardAdd
+          @click-next-button="$router.push('/')"
+          iconName="insert_drive_file"
+          heading="CSVファイルから追加"
+          text="CSVファイルに入っている授業データを追加します。"
+        ></CardAdd>
+      </div>
+      <div class="main__card">
+        <CardAdd
+          @click-next-button="$router.push('/')"
+          iconName="style"
+          heading="手動で授業を作成"
+          text="手入力でカスタマイズして授業を作成します。"
+        ></CardAdd>
+      </div>
     </div>
   </div>
 </template>
@@ -45,7 +55,6 @@ import CardAdd from "~/components/CardAdd.vue";
 import IconButton from "~/components/IconButton.vue";
 import PageHeader from "~/components/PageHeader.vue";
 import { defineComponent } from "vue";
-import { useRouter } from "vue-router";
 import { useStore } from "~/store";
 
 export default defineComponent({
@@ -56,7 +65,6 @@ export default defineComponent({
     PageHeader,
   },
   setup: () => {
-    const router = useRouter();
     const store = useStore();
 
     /** ヘッダー */
@@ -64,14 +72,8 @@ export default defineComponent({
       store.commit("setSidebar", !store.state.sidebar);
     };
 
-    /** メインコンテンツ */
-    const moveTo = async (path: string) => {
-      await router.push(path);
-    };
-
     return {
       CardAdd,
-      moveTo,
       IconButton,
       PageHeader,
       toggleSidebar,
@@ -91,6 +93,12 @@ export default defineComponent({
   max-width: $max-pc-item-width;
 }
 
+.header__left-btn {
+  @include wide-screen {
+    display: none;
+  }
+}
+
 .main {
   display: flex;
   flex-direction: column;
@@ -99,6 +107,8 @@ export default defineComponent({
   @include tab-and-pc {
     margin: $spacing-4 $spacing-0 $spacing-0;
   }
-  gap: $spacing-4 $spacing-0;
+  &__card {
+    margin-bottom: $spacing-4;
+  }
 }
 </style>
