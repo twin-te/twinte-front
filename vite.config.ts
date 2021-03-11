33d7645a7e4c5991a2d74b3c1694471df1d3b0c9
HikaruEgashira <shighten@gmail.com>
2021-03-11 09:09:23 +0900

    fix deprecated option

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  alias: {
-    "~": path.resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
   },
 });
