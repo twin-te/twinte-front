ec989face2fe09d0474f37d17caa15069de39a9d
Hayato Shishido <hayabusax555@yahoo.co.jp>
2021-03-20 23:11:01 +0900

    rename
    ・getRegisteredCourse -> getCourseById
    ・deleteRegisteredCourse -> deleteCourse

--- src/usecases/deleteCourse.ts
+++ src/usecases/deleteCourse.ts
@@ -4,9 +4,7 @@ import { Ports } from "~/adapter";
  * idに該当する登録済みの講義を削除する。
  */
 
-export const deleteRegisteredCourse = ({ api }: Ports) => async (
-  id: string
-) => {
+export const deleteCourse = ({ api }: Ports) => async (id: string) => {
   try {
     await api.registered_courses._id(id).$delete();
   } catch (error) {
