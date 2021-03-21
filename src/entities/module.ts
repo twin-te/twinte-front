4856a45a4e08142a2c60eda70859834ec8607a14
Hayato Shishido <hayabusax555@yahoo.co.jp>
2021-03-21 14:17:22 +0900

    add entities
    ・day.ts
    ・module.ts
    ・table.ts

--- src/entities/module.ts
+++ src/entities/module.ts
@@ -26,10 +26,59 @@ export const moduleMap: { [key in string]: ModuleJa } = {
   FallB: "秋B",
   FallC: "秋C",
 };
+export const courseModules: CourseModule[] = [
+  "Annual",
+  "SpringA",
+  "SpringB",
+  "SpringC",
+  "SummerVacation",
+  "FallA",
+  "FallB",
+  "FallC",
+  "SpringVacation",
+];
 
 export const modulesNum = (module: CourseModule): number =>
   modules.indexOf(module);
+export const courseModulesNum = (module: CourseModule) =>
+  courseModules.indexOf(module);
 export const moduleToJa = (module: CourseModule): ModuleJa =>
   moduleMap[module] ?? "春A";
 export const moduleFromJa = (moduleJa: ModuleJa): CourseModule =>
   modules[moduleJaList.indexOf(moduleJa)];
+
+/** CourseModulesに対応 */
+export type ModuleFlg = [
+  boolean,
+  boolean,
+  boolean,
+  boolean,
+  boolean,
+  boolean,
+  boolean,
+  boolean,
+  boolean
+];
+
+export const moduleFlgToDisplay = (modules: ModuleFlg): string[] => {
+  const result = [] as string[];
+  if (modules[0]) result.push("通年");
+  if (modules.slice(1, 4).some((b) => b)) {
+    result.push(
+      modules.slice(1, 4).reduce((a, c, i) => {
+        return modules[1 + i] ? a + ["A", "B", "C"][i] : a;
+      }, "春")
+    );
+  }
+  if (modules[4]) result.push("夏休");
+  if (modules.slice(5, 8).some((b) => b)) {
+    result.push(
+      modules.slice(5, 8).reduce((a, c, i) => {
+        return modules[5 + i] ? a + ["A", "B", "C"][i] : a;
+      }, "秋")
+    );
+  }
+  if (modules[8]) result.push("春休");
+
+  return result;
+};
--- src/entities/table.ts
+++ src/entities/table.ts
@@ -1,5 +1,5 @@
-import { Day } from "~/api/@types";
-import { weeksNum } from "./day";
+import { Day, RegisteredCourse } from "~/api/@types";
+import { SpecialDay, weeksNum } from "./day";
 
 export const getValue = <T = unknown>(
   target: T[][],
@@ -13,6 +13,13 @@ export type CourseState = {
   courseId: string;
 };
 
+export type SpecialCourse = {
+  id: string;
+  name: string;
+  module: string[];
+  room: string;
+};
+
 /**
  * ```
  * table
@@ -22,6 +29,10 @@ export type CourseState = {
  * ```
  */
 export type Table = CourseState[][][];
+
+export type SpecialTable = {
+  [key in SpecialDay]: SpecialCourse[];
+};
 
 export const dummyData: { table: Table } = {
   table: [
@@ -42,3 +53,250 @@ export const dummyData: { table: Table } = {
     [[], [], [], [], [], []],
   ],
 };
+
+const course1: RegisteredCourse = {
+  id: "xxx",
+  userId: "xxx",
+  year: 0,
+  name: "初等社会特論",
+  instructor: "xxx",
+  credit: 0,
+  methods: ["FaceToFace"],
+  schedules: [
+    {
+      module: "SummerVacation",
+      day: "Intensive",
+      period: 0,
+      room: "8B204",
+    },
+  ],
+  memo: "",
+  attendance: 0,
+  absence: 0,
+  late: 0,
+  tags: [],
+};
+const course2: RegisteredCourse = {
+  id: "xxx",
+  userId: "xxx",
+  year: 0,
+  name: "日本美術特別技法演習-1",
+  instructor: "xxx",
+  credit: 0,
+  methods: ["FaceToFace"],
+  schedules: [
+    {
+      module: "SpringB",
+      day: "Intensive",
+      period: 0,
+      room: "アート&デザイン実習室",
+    },
+    {
+      module: "SpringC",
+      day: "Intensive",
+      period: 0,
+      room: "アート&デザイン実習室",
+    },
+    {
+      module: "SummerVacation",
+      day: "Intensive",
+      period: 0,
+      room: "アート&デザイン実習室",
+    },
+  ],
+  memo: "",
+  attendance: 0,
+  absence: 0,
+  late: 0,
+  tags: [],
+};
+const course3: RegisteredCourse = {
+  id: "xxx",
+  userId: "xxx",
+  course: {
+    id: "xxx",
+    year: 0,
+    code: "xxx",
+    name: "原子核実験物理学II",
+    instructor: "xxx",
+    credit: 0,
+    overview: "xxx",
+    remarks: "xxx",
+    recommendedGrades: [0],
+    methods: ["FaceToFace"],
+    schedules: [
+      {
+        module: "FallA",
+        day: "Intensive",
+        period: 0,
+        room: "自然B118",
+      },
+      {
+        module: "FallB",
+        day: "Intensive",
+        period: 0,
+        room: "自然B118",
+      },
+      {
+        module: "FallC",
+        day: "Intensive",
+        period: 0,
+        room: "自然B118",
+      },
+    ],
+  },
+  year: 0,
+  memo: "",
+  attendance: 0,
+  absence: 0,
+  late: 0,
+  tags: [],
+};
+const course4: RegisteredCourse = {
+  id: "xxx",
+  userId: "xxx",
+  course: {
+    id: "xxx",
+    year: 0,
+    code: "xxx",
+    name: "xxx",
+    instructor: "xxx",
+    credit: 0,
+    overview: "xxx",
+    remarks: "xxx",
+    recommendedGrades: [0],
+    methods: ["FaceToFace"],
+    schedules: [
+      {
+        module: "Annual",
+        day: "Appointment",
+        period: 0,
+        room: "研究室",
+      },
+    ],
+  },
+  year: 0,
+  name: "応用動物昆虫学特別研究I",
+  memo: "",
+  attendance: 0,
+  absence: 0,
+  late: 0,
+  tags: [],
+};
+const course5: RegisteredCourse = {
+  id: "xxx",
+  userId: "xxx",
+  year: 0,
+  name: "国際生命産業科学インターンシップ",
+  instructor: "xxx",
+  credit: 0,
+  methods: ["FaceToFace"],
+  schedules: [
+    {
+      module: "Annual",
+      day: "Appointment",
+      period: 0,
+      room: "",
+    },
+  ],
+  memo: "",
+  attendance: 0,
+  absence: 0,
+  late: 0,
+  tags: [],
+};
+const course6: RegisteredCourse = {
+  id: "xxx",
+  userId: "xxx",
+  year: 0,
+  name: "International Conference Seminar I",
+  instructor: "xxx",
+  credit: 0,
+  methods: ["FaceToFace"],
+  schedules: [
+    {
+      module: "Annual",
+      day: "AnyTime",
+      period: 0,
+      room: "",
+    },
+  ],
+  memo: "",
+  attendance: 0,
+  absence: 0,
+  late: 0,
+  tags: [],
+};
+const course7: RegisteredCourse = {
+  id: "xxx",
+  userId: "xxx",
+  year: 0,
+  name: "International Conference Seminar I",
+  instructor: "xxx",
+  credit: 0,
+  methods: ["FaceToFace"],
+  schedules: [
+    {
+      module: "SpringA",
+      day: "AnyTime",
+      period: 0,
+      room: "情報理工後期特別研究室",
+    },
+    {
+      module: "SpringB",
+      day: "AnyTime",
+      period: 0,
+      room: "情報理工後期特別研究室",
+    },
+    {
+      module: "SpringC",
+      day: "AnyTime",
+      period: 0,
+      room: "情報理工後期特別研究室",
+    },
+  ],
+  memo: "",
+  attendance: 0,
+  absence: 0,
+  late: 0,
+  tags: [],
+};
+const course8: RegisteredCourse = {
+  id: "xxx",
+  userId: "xxx",
+  year: 0,
+  name: "色彩学",
+  instructor: "xxx",
+  credit: 0,
+  methods: ["FaceToFace"],
+  schedules: [
+    {
+      module: "SpringC",
+      day: "AnyTime",
+      period: 0,
+      room: "3A棟",
+    },
+    {
+      module: "SummerVacation",
+      day: "AnyTime",
+      period: 0,
+      room: "中央図書館",
+    },
+  ],
+  memo: "",
+  attendance: 0,
+  absence: 0,
+  late: 0,
+  tags: [],
+};
+
+export const dummySpecial: RegisteredCourse[] = [
+  course1,
+  course2,
+  course3,
+  course4,
+  course5,
+  course6,
+  course7,
+  course8,
+];
