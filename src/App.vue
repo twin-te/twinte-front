022bd63f526d30035a4d26f55f838de884a78ddb
HosokawaRyunosuke <thin.river.jp@gmail.com>
2021-03-30 03:28:52 +0900

    トップレベルのエラーを仮実装

--- src/App.vue
+++ src/App.vue
@@ -13,5 +13,10 @@ import Layout from "~/templates/Layout.vue";
 export default defineComponent({
   name: "App",
   components: { Layout },
+  errorCaptured: (error) => {
+    console.error(error);
+    // TODO: トップレベルのエラーの処理を実装する 
+    alert("エラーが発生しました。")
+  },
 });
 </script>
