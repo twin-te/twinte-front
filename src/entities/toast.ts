6c68d79e9ae763440d545099965b932e2022450b
HosokawaRyunosuke <thin.river.jp@gmail.com>
2021-03-30 03:57:26 +0900

    トーストが自動で消えないオプションを付与

--- src/entities/toast.ts
+++ src/entities/toast.ts
@@ -5,6 +5,10 @@ export type Toast = {
   id: number;
 };
 
+/**
+ * トースト通知を表示する。指定した ms 後に消える。
+ * 0を指定した場合は自動で消えない。
+ */
 export const displayToast = ({ store }: Ports) => (
   text: string,
   displayPeriod = 5000
@@ -13,7 +17,9 @@ export const displayToast = ({ store }: Ports) => (
   const id = Math.round(Math.random() * 100);
   console.log({ id, text });
   store.commit("addToast", { id, text });
-  setTimeout(() => {
-    store.commit("deleteToast", id);
-  }, displayPeriod);
+  if (displayPeriod > 0) {
+    setTimeout(() => {
+      store.commit("deleteToast", id);
+    }, displayPeriod);
+  }
 };
