bf509cc42af60232b39766dbd10685a35a7d0c8f
HikaruEgashira <shighten@gmail.com>
2021-03-31 15:07:12 +0900

    fix path

--- src/usecases/getLoginUrl.ts
+++ src/usecases/getLoginUrl.ts
@@ -1,14 +1,13 @@
 export type Provider = "google" | "apple" | "twitter";
 
-export const authBaseURL =
-  import.meta.env.VITE_AUTH_URL === undefined
-    ? "https://api.dev.twinte.net/auth/v3"
-    : String(import.meta.env.VITE_AUTH_URL);
+export const authBaseURL = import.meta.env.DEV
+  ? "https://api.dev.twinte.net/auth/v3"
+  : `${window.location.origin}/auth/v3`;
 
 export const getLoginUrl = (provider: Provider) => {
-  return `${authBaseURL}/${provider}?redirect_url=https://app.dev.twinte.net`;
+  return `${authBaseURL}/${provider}?redirect_url=${window.location.origin}`;
 };
 
 export const getLogoutUrl = () => {
-  return `${authBaseURL}/logout?redirect_url=https://app.dev.twinte.net`;
+  return `${authBaseURL}/logout?redirect_url=${window.location.origin}`;
 };
