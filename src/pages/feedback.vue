cb88abd830247e28cdd20bebff8ac4daaa5d0093
HosokawaRyunosuke <thin.river.jp@gmail.com>
2021-04-04 08:27:27 +0900

    Screen Shot 送信機能を追加

--- src/pages/feedback.vue
+++ src/pages/feedback.vue
@@ -27,19 +27,19 @@
               v-model:selectedOption="feedbackType"
             ></Dropdown>
           </section>
-          <!-- <section class="feedback__row">
+          <section class="feedback__row">
             <Label value="スクリーンショットの添付"></Label>
             <div class="feedback__note">
-              バグ報告の場合はエラー画面のスクリーンショットをいただけると幸いです。
+              バグ報告の場合は不具合のある画面のスクリーンショットをいただけると幸いです。
             </div>
             <InputButtonFile
               name="csv-file"
-              @change-file="loadCourses"
+              @change-file="screenShot = $event"
               accept="image/*"
             >
               画像をアップロードする
             </InputButtonFile>
-          </section> -->
+          </section>
           <section class="feedback__row">
             <Label value="内容" :mandatory="true"></Label>
             <div class="feedback__note"></div>
@@ -54,11 +54,11 @@
             v-if="['バグの報告', 'お問い合わせ'].includes(feedbackType)"
           >
             <Label
-              value="メールアドレス"
+              value="連絡先メールアドレス or Twitterアカウント"
               :mandatory="feedbackType === 'お問い合わせ'"
             ></Label>
             <div class="feedback__note">{{ emailNote[feedbackType] }}</div>
-            <TextFieldSingleLine v-model="email"></TextFieldSingleLine>
+            <TextFieldSingleLine v-model="email" placeholder="xxx@example.com / @te_twin"></TextFieldSingleLine>
           </section>
         </div>
       </div>
@@ -79,27 +79,28 @@
 
 <script lang="ts">
 import { computed, defineComponent, ref } from "vue";
+import { displayToast } from "~/entities/toast";
+import { postScreenshot } from "~/usecases/sendFeedback";
 import { useHead } from "@vueuse/head";
-import axios from "axios";
+import { usePorts } from "~/usecases";
+import { useRouter } from "vue-router";
+import { useStore } from "~/store";
+import axios, { AxiosError } from "axios";
 import Button from "~/components/Button.vue";
 import Dropdown from "~/components/Dropdown.vue";
 import IconButton from "~/components/IconButton.vue";
-// import InputButtonFile from "~/components/InputButtonFile.vue";
+import InputButtonFile from "~/components/InputButtonFile.vue";
 import Label from "~/components/Label.vue";
 import PageHeader from "~/components/PageHeader.vue";
 import TextFieldMultilines from "~/components/TextFieldMultilines.vue";
 import TextFieldSingleLine from "~/components/TextFieldSingleLine.vue";
-import { useStore } from "~/store";
-import { displayToast } from "~/entities/toast";
-import { usePorts } from "~/usecases";
-import { useRouter } from "vue-router";
 
 export default defineComponent({
   components: {
     Button,
     Dropdown,
     IconButton,
-    // InputButtonFile,
+    InputButtonFile,
     Label,
     PageHeader,
     TextFieldMultilines,
@@ -115,12 +116,13 @@ export default defineComponent({
     });
 
     const feedbackType = ref("バグの報告");
+    const screenShot = ref<File>();
     const feedbackContent = ref("");
     const email = ref("");
 
     const placeholder = {
       バグの報告:
-        "例）授業の検索ページで「XXXX」という授業を追加しようとしたら、「OOOO」というエラーメッセージが出てきて追加できませんでした。",
+        "例）授業の検索ページで「XXXX」という授業を追加しようとしたら、「OOOO」というエラーメッセージが出てきて追加できませんでした。Pixel4a、Android11です。",
       新機能の要望:
         "例）学部生ですが、検索で大学院の授業ばかりでてきてしまうので大学院の授業を除外して検索する機能がほしいです。",
       お問い合わせ: "例）Twin:te の発音はツインテですか？トゥインテですか？",
@@ -128,8 +130,8 @@ export default defineComponent({
     };
     const emailNote = {
       バグの報告:
-        "より詳しい原因解明のため開発チームから連絡を差し上げる場合がございます。ご協力いただける場合はメールアドレスをご記入ください。",
-      お問い合わせ: "返信用のメールアドレスをご記入下さい。",
+        "より詳しい原因解明のため開発チームから連絡を差し上げる場合がございます。ご協力いただける場合はメールアドレスまたはTwitterアカウントをご記入ください。",
+      お問い合わせ: "返信用のメールアドレスまたはTwitterアカウントをご記入下さい。",
     };
 
     const btnStatus = computed(() => {
@@ -141,32 +143,51 @@ export default defineComponent({
       return "default";
     });
 
-    const sendFeedback = () => {
+    const sendFeedback = async () => {
+      displayToast(ports)("フィードバックを送信中…", {
+        type: "primary",
+      });
       const formData = new FormData();
       formData.append("entry.1670691903", store.getters.user.id);
       formData.append("entry.1912135146", feedbackType.value);
       formData.append("entry.240515057", feedbackContent.value);
       formData.append("entry.1261834897", email.value);
-      axios
-        .post(
+      let screenshotUrl = "";
+      try {
+        screenshotUrl = await postScreenshot(
+          screenShot.value,
+          store.getters.user.id
+        );
+      } catch (error) {
+        console.error(error);
+        displayToast(ports)(
+          "申し訳ございません。スクリーンショットの送信に失敗しました。ネットワーク環境をご確認ください。"
+        );
+        return;
+      }
+      formData.append("entry.1457360447", screenshotUrl);
+      try {
+        await axios.post(
           "https://docs.google.com/forms/u/0/d/e/1FAIpQLSecBhx7GHm870_BHEjm94NAKOoxJPzbTL-pxpvy6BqxQQh6ag/formResponse",
           formData
-        )
-        .then((res) => {
-          console.log(res);
+        );
+      } catch (error) {
+        // HACK: google form に POST する方法は確定で CORS エラーが出るが成功する
+        //       CORS エラーのときは status が undefined になるため無視する
+        if ((error as AxiosError).response?.status !== undefined) {
           displayToast(ports)(
-            "フィードバックを送信しました。ありがとうございます。",
-            {
-              type: "primary",
-            }
+            "申し訳ございません。フィードバックの送信に失敗しました。ネットワーク環境をご確認ください。"
           );
-          router.push("/");
-        })
-        .catch(() => {
-          displayToast(ports)(
-            "申し訳ございません。送信に失敗しました。ネットワーク環境をご確認ください。"
-          );
-        });
+          return;
+        }
+      }
+      displayToast(ports)(
+        "フィードバックを送信しました。ありがとうございます。",
+        {
+          type: "primary",
+        }
+      );
+      router.push("/");
     };
 
     return {
@@ -176,6 +197,7 @@ export default defineComponent({
       feedbackContent,
       feedbackType,
       placeholder,
+      screenShot,
       sendFeedback,
     };
   },
