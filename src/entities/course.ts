10f30b77caf9ca638b0fa3053c408ffd317a5303
Hayato Shishido <hayabusax555@yahoo.co.jp>
2021-04-03 20:56:53 +0900

    fix 開講時限への変換をperiodToStringに変更

--- src/entities/course.ts
+++ src/entities/course.ts
@@ -9,6 +9,7 @@ import {
   Schedule,
   scheduleToApi,
 } from "~/entities/schedule";
+import { periodToString } from "~/usecases/periodToString";
 
 export type DisplayCourse = {
   code: string;
@@ -62,7 +63,7 @@ export const apiToDisplayCourse = (
     char
   ),
   date: blankToChar(
-    getLectureTimeAsStr(
+    periodToString(
       registeredCourse?.schedules ?? registeredCourse.course?.schedules ?? []
     ),
     char
@@ -99,75 +100,6 @@ export const apiToDisplayCourse = (
   ),
   registeredCourse,
 });
-
-// getLectureTimeAsStr.ts
-
-// 2次元配列を転置
-const transpose = (matrix: string[][]) =>
-  matrix[0].map((_, i) => matrix.map((r) => r[i]));
-
-// 1. string[][1]の重複した要素を取り除く
-// Ex) [['月', '56'], ['火', '56']] -> [['月', ''], ['火', '56']]
-// 2. string[][]の要素を結合して文字列に変換する
-// Ex) [['月', ''], ['火', '56']] -> '月火56'
-const reduceArr = (arr: string[][]) => {
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i - 1][1] == arr[i][1]) {
-      arr[i - 1][1] = "";
-    }
-  }
-  return arr.reduce((acc, cur) => {
-    return `${acc}${cur[0]}${cur[1]}`;
-  }, "");
-};
-
-const genTreeStrucStr = (
-  startIdx: number,
-  endIdx: number,
-  rows: string[][],
-  level: number
-): string => {
-  if (level == 4) {
-    return "";
-  }
-  const arr = rows[level];
-  const letters: string[][] = [];
-  let nextStart = startIdx;
-  for (let i = startIdx; i <= endIdx; i++) {
-    if (i == endIdx || arr[i] != arr[i + 1]) {
-      const s = genTreeStrucStr(nextStart, i, rows, level + 1);
-      if (typeof s !== "undefined") {
-        letters.push([arr[i], s]);
-      } else {
-        letters.push([arr[i], ""]);
-      }
-      nextStart = i + 1;
-    }
-  }
-  return reduceArr(letters);
-};
-
-const getLectureTimeAsStr = (schedules: CourseSchedule[]): string => {
-  if (schedules.length > 0) {
-    const li = schedules
-      .map((x) => ({ ...x, module: moduleToJa(x.module), day: dayToJa(x.day) }))
-      .map((x): string[] => {
-        // 0時限目は''と扱う
-        const period = x.period === 0 ? "" : String(x.period);
-        return [x.module[0], x.module[1], x.day, period];
-      });
-    const translocation = transpose(li);
-    const txt = genTreeStrucStr(
-      0,
-      translocation[0].length - 1,
-      translocation,
-      0
-    );
-    return txt;
-  } else {
-    return "";
-  }
-};
 
 /**
  * 日本語の学期名などを api の形式に合うようにフォーマットする
