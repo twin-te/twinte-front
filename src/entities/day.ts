5acfe39ef514135a629d36bc86204f71dee3f455
Hayato Shishido <hayabusax555@yahoo.co.jp>
2021-04-05 21:10:32 +0900

    add fullDayJa

--- src/entities/day.ts
+++ src/entities/day.ts
@@ -5,14 +5,22 @@ import { Day } from "~/api/@types";
  * 基本は、英語表記の型は Day を用いる。（他の英語表記の型を作るとややこしくなるため）
  */
 
-// 通常
+export type SpecialDay = "Intensive" | "Appointment" | "AnyTime";
+
 export type WeekDayJa = "月" | "火" | "水" | "木" | "金";
 export type DayJa = "月" | "火" | "水" | "木" | "金" | "土" | "日";
 export type NotWeekDay = "その他";
 export type ScheduleDayJa = DayJa | NotWeekDay | "指定なし";
+export type SpecialDayJa = "集中" | "応談" | "随時";
+export type FullDayJa = DayJa | SpecialDayJa;
 
 export const weekdays: Day[] = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 export const week: Day[] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+export const specialDays: SpecialDay[] = [
+  "Intensive",
+  "Appointment",
+  "AnyTime",
+];
 export const fullDays: Day[] = [
   "Mon",
   "Tue",
@@ -56,6 +64,23 @@ export const weekMap: Partial<Record<Day, DayJa>> = {
   Sat: "土",
   Sun: "日",
 };
+export const specialDayMap: Record<SpecialDay, SpecialDayJa> = {
+  Intensive: "集中",
+  Appointment: "応談",
+  AnyTime: "随時",
+};
+export const fullDayMap: Partial<Record<Day, FullDayJa>> = {
+  Mon: "月",
+  Tue: "火",
+  Wed: "水",
+  Thu: "木",
+  Fri: "金",
+  Sat: "土",
+  Sun: "日",
+  Intensive: "集中",
+  Appointment: "応談",
+  AnyTime: "随時",
+};
 
 export const weekdayNum = (day: Day): number => weekdays.indexOf(day);
 export const weekNum = (day: Day): number => week.indexOf(day);
@@ -67,17 +92,5 @@ export const jaToDay = (ja: DayJa): Day =>
   week.find((key) => weekMap[key] === ja) ?? "Unknown";
 export const scheduleJaToDay = (ja: ScheduleDayJa): Day =>
   week.find((key) => weekMap[key] === ja) ?? "Unknown";
-
-// 特殊
-export type SpecialDay = "Intensive" | "Appointment" | "AnyTime";
-export type SpecialDayJa = "集中" | "応談" | "随時";
-export const specialDays: SpecialDay[] = [
-  "Intensive",
-  "Appointment",
-  "AnyTime",
-];
-export const specialDayMap: Record<SpecialDay, SpecialDayJa> = {
-  Intensive: "集中",
-  Appointment: "応談",
-  AnyTime: "随時",
-};
+export const fullDayJaToDay = (ja: FullDayJa): Day =>
+  fullDays.find((key) => fullDayMap[key] === ja) ?? "Unknown";
