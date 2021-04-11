2f3675135e14c6ed93c9f90bcd56ccf92d2dc85b
Mimori256 <mimorimori256@gmail.com>
2021-04-11 09:32:32 +0900

    出欠カウントが負にならないようにする

--- src/usecases/updateCourse.ts
+++ src/usecases/updateCourse.ts
@@ -2,6 +2,7 @@ import { isValidStatus } from "~/usecases/api";
 import { NetworkAccessError, NetworkError } from "~/usecases/error";
 import { Ports } from "~/adapter";
 import { RegisteredCourse } from "~/api/@types";
+import { validateCourse } from "~/entities/course";
 
 /**
  * 出欠カウントを更新する。
@@ -9,9 +10,10 @@ import { RegisteredCourse } from "~/api/@types";
 export const updateCourse = ({ api, store }: Ports) => async (
   course: Required<RegisteredCourse>
 ): Promise<RegisteredCourse> => {
+  const validCourse = validateCourse(course);
   const { body, status, originalResponse } = await api.registered_courses
     ._id(course.id)
-    .put({ body: course })
+    .put({ body: validCourse })
     .catch(() => {
       throw new NetworkError();
     });
