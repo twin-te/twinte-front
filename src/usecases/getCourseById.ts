be3c69ab6f31adace1cad0c6d27086907038cc34
Hayato Shishido <hayato24sh@gmail.com>
2022-04-02 15:08:45 +0900

    Add tag in course defail page

--- src/usecases/getCourseById.ts
+++ src/usecases/getCourseById.ts
@@ -1,16 +1,17 @@
 import { RegisteredCourse } from "~/api/@types";
-import { store } from "~/store";
-import { reactive, ToRefs, toRefs } from "vue";
 import { isValidStatus } from "~/usecases/api";
 import { NetworkError, NetworkAccessError } from "~/usecases/error";
 import { Ports } from "~/adapter";
+import { getCourseListByYear } from "./getCourseListByYear";
 
 /**
  * storeまたはAPIからidに該当する登録した講義データを取得する。
  */
-export const getCourseById = ({ api }: Ports) => async (
+export const getCourseById = (ports: Ports) => async (
   id: string
 ): Promise<RegisteredCourse> => {
+  const { api, store } = ports;
+
   const storedCourse = (store.getters.courses as Array<RegisteredCourse>).find(
     (c) => id === c.id
   );
@@ -22,10 +23,13 @@ export const getCourseById = ({ api }: Ports) => async (
     .catch(() => {
       throw new NetworkError();
     });
-  if (isValidStatus(status)) {
-    return body;
-  } else {
+  if (!isValidStatus(status)) {
     console.error(body);
     throw new NetworkAccessError(originalResponse);
   }
+  // store.state.courses において year が同じ授業の初期化を行う
+  const courses = getCourseListByYear(ports)(body.year);
+  store.commit("setCourses", { year: body.year, courses });
+
+  return body;
 };
