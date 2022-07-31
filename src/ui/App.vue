012c46cd972efd1bbab1d904131f9302675aaae2
Hayato Shishido <engr.hayato24s@gmail.com>
2022-08-01 00:58:05 +0900

    add ui

---
+++ src/ui/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Hello World!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue-demi";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
