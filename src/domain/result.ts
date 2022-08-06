99dcddff8f5334cf694b292b07e4e01d79856e1f
Hayato Shishido <engr.hayato24s@gmail.com>
2022-08-06 21:37:27 +0900

    update result and errors

--- src/domain/result.ts
+++ src/domain/result.ts
@@ -1,60 +1,61 @@
-export class BaseError extends Error {
-  constructor(message?: string) {
-    super(message);
-    this.name = new.target.name;
-  }
+/**
+ * An argument has right type, but an inappropriate value.
+ */
+export class ValueError extends Error {
+  readonly name = "ValueError";
 }
 
-export class NotFoundError extends BaseError {
-  constructor(message: string) {
-    super(message);
-  }
+/**
+ * The resource is not found.
+ */
+export class NotFoundError extends Error {
+  readonly name = "NotFoundError";
 }
 
-export class ValueError extends BaseError {
-  constructor(message: string) {
-    super(message);
-  }
+/**
+ * When unauthorized user try to access resource that only authorized user can access, this error occurs.
+ */
+export class UnauthorizedError extends Error {
+  readonly name = "UnauthorizedError";
 }
 
-export class NetworkError extends BaseError {}
+export class NetworkError extends Error {
+  readonly name = "NetworkError";
+}
 
-export class InternalServerError extends BaseError {
-  constructor(message: string) {
-    super(message);
-  }
+export class InternalServerError extends Error {
+  readonly name = "InternalServerError";
 }
 
 export type ResultError =
-  | NotFoundError
   | ValueError
+  | NotFoundError
+  | UnauthorizedError
   | NetworkError
   | InternalServerError;
 
-export class Ok<T, E extends ResultError> {
+export class Ok<T = unknown> {
   constructor(readonly value: T) {}
-  isOk(): this is Ok<T, E> {
+  type = "ok" as const;
+  isOk(): this is Ok<T> {
     return true;
   }
-  isErr(): this is Err<T, E> {
+  isErr(): this is Err {
     return false;
   }
 }
 
-export class Err<T, E extends ResultError> {
+export class Err<E = ResultError> {
   constructor(readonly value: E) {}
-  isOk(): this is Ok<T, E> {
+  type = "err" as const;
+  isOk(): this is Ok {
     return false;
   }
-  isErr(): this is Err<T, E> {
+  isErr(): this is Err<E> {
     return true;
   }
 }
 
-export type Result<T = unknown, E extends ResultError = ResultError> =
-  | Ok<T, E>
-  | Err<T, E>;
+export type Result<T, E extends ResultError> = Ok<T> | Err<E>;
 
-export type PromiseResult<T, E extends ResultError = ResultError> = Promise<
-  Result<T, E>
->;
+export type PromiseResult<T, E extends ResultError> = Promise<Result<T, E>>;
